28cb16d722e94717c2830bdb5c750959f88cca35
Vatsal Sanchala <bevatsal1122@gmail.com>
2022-10-08 02:36:00 +0530

    Add files via upload

---
+++ OOP/JaggedArrays3D.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class JaggedArrays3D {
+   public static void main(String[] args) {
+      Scanner s=new Scanner(System.in);
+      char data[][][]=new char [2][][];
+      data[0]=new char[3][];
+      data[0][0]=new char[4]; 
+      data[0][1]=new char[3]; 
+      data[0][2]=new char[2]; 
+      data[1]=new char[2][];
+      data[1][0]=new char[3]; 
+      data[1][1]=new char[4]; 
+      for (int i=0; i<2; ++i) {
+         for (int j=0; j<data[i].length; ++j) {
+            for (int k=0; k<data[i][j].length; ++k) {
+               data[i][j][k]='*';
+            }
+         }
+      }
+
+      System.out.println();
+      for (int i=0; i<2; ++i) {
+         for (int j=0; j<data[i].length; ++j) {
+            for (int k=0; k<data[i][j].length; ++k) System.out.print(data[i][j][k]+" ");
+            System.out.println();
+         }
+         System.out.println();
+      }
+
+   }
+}
