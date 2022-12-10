6fad3701d03a4033cf777693945d25fd6cee9445
Vatsal Sanchala <bevatsal1122@gmail.com>
2022-12-11 00:59:36 +0530

    Add files via upload

--- DS/PriorityQueue.c
+++ DS/PriorityQueue.c
@@ -19,14 +19,6 @@ void pushAtStart(struct node **start, char name[], int age, int disability, int 
   *start=insertNode;
 }
 
-void pushAtEnd(struct node **start, int data) {
-   struct node *insertNode=malloc(sizeof(struct node));
-   struct node *q=*start;
-   while (q->next!=NULL) q=q->next;
-   q->next=insertNode;
-   insertNode->next=NULL;
-}
-
 void pushInBetween(struct node **start, char name[], int age, int disability, int priority, int position) {
    if (position==0) {
       pushAtStart(start, name, age, disability, priority);
@@ -58,7 +50,7 @@ int search(struct node *start, int searchData) {
 }
 
 void display(struct node *start) {
-   printf("\nServicing Order (Adjusted according to Priority Queue, To be removed from bottom) - \n");
+   printf("\nServicing Order (Adjusted according to Priority Queue, To be removed from top) - \n");
    while (start!=NULL) {
       printf("%s", start->name);
       printf("\tAge: %d ", start->age);
@@ -72,33 +64,31 @@ void display(struct node *start) {
 
 int main()
 {
-    struct node *top=NULL;
-    int choice;
-    printf("\nEnter 1 for Yes & 0 for No\nEnter Choice: ");
-    scanf("%d", &choice);
-    while (choice) {
-         int age, disabled, priority=0, position=0;
-         char name[1000];
-         printf("\nAdd Person Name: ");
-         scanf("%s", name);
-         printf("Add Person Age: ");
-         scanf("%d", &age);
-         printf("\nEnter 1 for Yes & 0 for No\nIs Person disabled ?? ");
-         scanf("%d", &disabled);
-         if (disabled==1 && age>=60) priority=3;
-         else if (disabled==1) priority=2;
-         else if (age>=60) priority=1;
-         else priority=0;
-         struct node *start=top;
-         while (start!=NULL && priority>=start->priority) {
-            position++;
-            start=start->next;
-         }
-         pushInBetween(&top, name, age, disabled, priority, position);
-         printf("\nEnter 1 for Yes & 0 for No\nEnter Choice: ");
-         scanf("%d", &choice);
+   struct node *top=NULL;
+   int choice;
+   printf("\nEnter 1 for Yes & 0 for No\nAdd Person ?? ");
+   scanf("%d", &choice);
+   while (choice) {
+      int age, disabled, priority=0, position=0;
+      char name[1000];
+      printf("\nAdd Person Name: ");
+      scanf("%s", name);
+      printf("Add Person Age: ");
+      scanf("%d", &age);
+      printf("\nEnter 1 for Yes & 0 for No\nIs Person disabled ?? ");
+      scanf("%d", &disabled);
+      if (disabled==1 && age>=60) priority=3;
+      else if (disabled==1) priority=2;
+      else if (age>=60) priority=1;
+      struct node *start=top;
+      while (start!=NULL && priority<=start->priority) {
+         position++;
+         start=start->next;
+      }
+      pushInBetween(&top, name, age, disabled, priority, position);
+      printf("\nEnter 1 for Yes & 0 for No\nAdd Person ?? ");
+      scanf("%d", &choice);
    }
    display(top);
-   
    return 0;
 }
